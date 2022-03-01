a911894ec10481805595369d8b6e49cd7b9c7e68
Evis Chang <evis.a.chang@accenture.com>
2022-03-01 22:00:31 +0800

    init project

---
+++ src/main/java/com/project/selectfood/SelectfoodApplication.java
@@ -0,0 +1,13 @@
+package com.project.selectfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectfoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectfoodApplication.class, args);
+    }
+
+}
