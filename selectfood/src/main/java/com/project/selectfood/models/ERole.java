107d6f1bd98e65f1c983bb326459cf0cba2400fc
Evis Chang <evis.a.chang@accenture.com>
2022-04-24 10:10:11 +0800

    add security and change to use H2 db instead of postgres

---
+++ selectfood/src/main/java/com/project/selectfood/models/ERole.java
@@ -0,0 +1,7 @@
+package com.project.selectfood.models;
+
+public enum ERole {
+  ROLE_USER,
+  ROLE_MODERATOR,
+  ROLE_ADMIN
+}
---
+++ selectfood/src/main/java/com/project/selectfood/models/Role.java
@@ -0,0 +1,23 @@
+package com.project.selectfood.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "roles")
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(length = 20)
+  private ERole name;
+
+}
